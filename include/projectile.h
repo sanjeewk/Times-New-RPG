255fc4764eb3c734313c5eb2106846ecda8823a5
Sanjeewk <sanjeew.kanagaraj@gmail.com>
2025-03-21 19:11:34 +0800

    [feat]: add projectile class

---
+++ include/projectile.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include "raylib.h"
+#include "raymath.h"
+
+struct Projectile
+{
+    Vector2 position;
+    Vector2 direction;
+    float speed;
+    bool active;
+};
+
+void update_projectile(Projectile& projectile);
--- src/projectile.cpp
+++ src/projectile.cpp
@@ -1,10 +1,39 @@
 #include <vector>
 #include "raylib.h"
+#include "raymath.h"
+#include "projectile.h"
+
+constexpr int scWidth = 1100;
+constexpr int scHeight = 950;
 
 // Projectile structure
-struct Projectile {
-    Vector2 position;
-    Vector2 direction;
-    float speed;
-    bool active;
-};
+
+
+void update_projectile(Projectile& projectile) {
+    if (projectile.active) {
+        projectile.position = Vector2Add(projectile.position,
+            Vector2Scale(projectile.direction, projectile.speed));
+
+        // Deactivate projectiles that go off-screen
+        if (projectile.position.x < 0 || projectile.position.x > scWidth ||
+            projectile.position.y < 0 || projectile.position.y > scHeight) {
+            projectile.active = false;
+        }
+    }
+}
+
+// void collisions() {
+//     // projectile collisions
+//     for (auto& projectile : projectiles) {
+//         if (CheckCollisionCircles(projectile.position, projectileRadius,
+//             enemy.position, enemy.radius)) {
+//             projectile.active = false;
+//             // Handle enemy hit
+//         }
+//     }
+// }
+
+//void remove_projectiles(){
+//    projectiles.erase(std::remove_if(projectiles.begin(), projectiles.end(),
+//    [](const Projectile& p) { return !p.active; }), projectiles.end());
+//}
