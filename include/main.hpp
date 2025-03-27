865b70dc0a19d27a7b8421763de074d7f4bf322e
Sanjeewk <sanjeew.kanagaraj@gmail.com>
2025-03-27 16:53:29 +0800

    [feat]: move game class to header file

--- include/main.hpp
+++ include/main.hpp
@@ -5,23 +5,22 @@
 #include "raylib.h"
 #include "raymath.h"
 
-#include "tilemap.hpp"
+#include "display.hpp"
 #include "audio.hpp"
 #include "projectile.hpp"
 #include "player.hpp"
 
-constexpr int TILE_WIDTH = 16;
-constexpr int TILE_HEIGHT = 16;
-constexpr int screenWidth = 1100;
-constexpr int screenHeight = 950;
 
 enum class GameState {Pause, Game};
 enum class TextureAsset { Tilemap };
 
+// function to get if coordinates are outside the bound
+// enemy class
+
 class Timer {
 public:
-    double startTime;
-    double lifeTime;
+    double startTime = 0;
+    double lifeTime = 0;
     bool isActive = false;
 
     void Start(double lifetime) {
@@ -37,4 +36,51 @@ public:
     double GetElapsed() const {
         return GetTime() - startTime;
     }
+
 };
+
+
+class Game {
+    private:
+        static constexpr int MAX_TEXTURES = 1;
+        static constexpr int WORLD_WIDTH = 25;
+        static constexpr int WORLD_HEIGHT = 20;
+    
+        std::array<Texture2D, MAX_TEXTURES> textures;
+    
+    // world stored in this 2D arry
+        Tile world[WORLD_WIDTH][WORLD_HEIGHT];
+        Tile dungeon[WORLD_WIDTH][WORLD_HEIGHT];
+        Camera2D camera;
+    
+        Entity player;
+        Entity dungeon_gate;
+        Entity orc;
+        Entity chest;
+    
+        Timer combatTextTimer;
+        Timer playerTimer;
+    
+        Audio audio;
+        int player_sprite_toggle = 0;
+        int bound = 8;
+        int wall_width = 3;
+    
+        std::vector<Projectile> projectiles;
+        const float projectileSpeed = 5.0f;
+        const float projectileRadius = 2.0f;
+    
+    
+    public:
+        Game();
+    
+        void Startup();
+    
+        void Update();
+    
+        void Render();
+    
+        void Shutdown();    
+    // private:
+        void DrawTile(int pos_x, int pos_y, int texture_index_x, int texture_index_y, int flip=0);
+    };
