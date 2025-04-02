618eed228e0fe760b7fed0bd2f550856c4986139
Sanjeewk <sanjeew.kanagaraj@gmail.com>
2025-04-02 14:46:54 +0530

    [ref]: move world width and height to display.hpp

--- include/display.hpp
+++ include/display.hpp
@@ -4,6 +4,10 @@ constexpr int TILE_HEIGHT = 16;
 constexpr int screenWidth = 1100;
 constexpr int screenHeight = 950;
 
+constexpr int MAX_TEXTURES = 3;
+constexpr int WORLD_WIDTH = 20;
+constexpr int WORLD_HEIGHT = 18;
+
 enum class TileType { Dirt, Grass, Tree, Boundary };
 
 struct Tile {
